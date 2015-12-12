c4d3b4b4e2ab8ddd3200bc265ac5c9d4103f46d4
Hinata2712 <s134147@hftl.de>
2015-12-12 18:52:50 +0100

    TomEE_Prototype_V0.1
    -include Maria DB
    -include JPA
    --include all relevant tables

---
+++ TomEE_Code/RefugEEApp/src/ra/model/WelcomeRevisionEntity.java
@@ -0,0 +1,12 @@
+package ra.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by the-l_000 on 12.12.2015.
+ */
+@Entity
+@Table(name = "welcome_revision", schema = "", catalog = "refugeewiki")
+public class WelcomeRevisionEntity {
+}
---
+++ TomEE_Code/RefugEEApp/src/ra/model/WelcomeTranslateStashEntity.java
@@ -0,0 +1,12 @@
+package ra.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by the-l_000 on 12.12.2015.
+ */
+@Entity
+@Table(name = "welcome_translate_stash", schema = "", catalog = "refugeewiki")
+public class WelcomeTranslateStashEntity {
+}
